102db8d25f4d8f8fa2f34123adfa40be1e5adf15
eyangch <45834647+eyangch@users.noreply.github.com>
2021-07-14 09:23:48 -0500

    Create script.py

---
+++ Crypto/Retails Sales Associate/script.py
@@ -0,0 +1,18 @@
+from Crypto.Util.number import *
+
+
+with open('flag.txt','r') as f:
+    flag = f.read().strip()
+
+e=65537
+p=getPrime(512)
+q=getPrime(512)
+n=p*q
+m=bytes_to_long(flag)
+ct=pow(m,e,n)
+
+
+print (p)
+print (q)
+print (e)
+print (ct)
