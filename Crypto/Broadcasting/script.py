cd5f0a2461bba95e0b386e64c90bbbaccfd766b7
bever209 <74867461+bever209@users.noreply.github.com>
2021-07-14 10:37:29 -0400

    Update script.py

--- Crypto/Broadcasting/script.py
+++ Crypto/Broadcasting/script.py
@@ -4,15 +4,21 @@ with open('flag.txt','r') as f:
     flag = f.read().strip()
 
 m=bytes_to_long(flag)
-e=3
+e=17
+n_arr=[]
+ct_arr=[]
 
-for t in range(e):
+for t in range(17):
   p=getPrime(256)
   q=getPrime(256)
   n=p*q
 
   assert (n>m)
-    
   ct=pow(m,e,n)
-  print (ct)
-  print (n)
+  n_arr.append(n)
+  ct_arr.append(ct)
+  
+
+print (n_arr)
+print (ct_arr)
+
