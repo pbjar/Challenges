60b1130c7c937ae5fdf662fd27fa4e1468d1d3f2
eyangch <eyangch@gmail.com>
2021-09-11 18:56:51 -0500

    Add Rev/web and update flag.txt for other chals

---
+++ Rev/web/v1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int main(void){
+    CURL *curl;
+    CURLcode res;
+    FILE *fp;
+    char *nversion = "v2";
+    char *url = "http://137.184.108.185/v2";
+    curl = curl_easy_init();
+    if(curl){
+        fp = fopen(nversion, "wb");
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        curl_easy_cleanup(curl);
+        fclose(fp);
+    }
+    return 0;
+}
