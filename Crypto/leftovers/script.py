5cf915ed756cbb7103cca1ac8710c19d90c433fb
bever209 <74867461+bever209@users.noreply.github.com>
2021-09-16 11:43:08 -0400

    Update script.py

--- Crypto/leftovers/script.py
+++ Crypto/leftovers/script.py
@@ -27,16 +27,34 @@ for i in range(r):
   lst.append(0)
   
 lst=shuffle(lst)
-print ("All right, so the goal is that I will send you a list.")
-print ("Then you will send me a list of numbers back with the same size, separated by spaces.")
-print ("Now for each number in your list, a, if it satisfies a to the power of "+str(small)+" is congruent to 1 mod "+str(mod)+" I will give you the flag :yayy:")
-print (lst)
+print ("Alright, I will send you a string, where each char contains either 0 or 1.")
+print ("Then you will send me a list of integers back with the same size, separated by spaces.")
+print ("Now for each number in your list, a, if it satisfies a to the power of "+str(small)+" is congruent to 1 mod "+str(mod)+" it will encode to 1.")
+print ("Otherwise, your number will encode to 0. Now I will concattenate all of your encoded numbers into a string.")
+print ("If this string equals the original string I sent you, then you will get the flag :yayy:.")
+print ("One final caveat: all your numbers must be unique and positive integers greater than 1 and less than "+str(mod)+".")
+string=""
+for i in lst:
+  string+=str(i)
+print (string)
 
 s=input("Enter: ").strip().split()
-s=[int (i) for i in s]
-check=len(s)==len(lst)
-for i in range(len(s)):
-  check=check & s[i]==lst[i]
+s=[int(i) for i in s]
+check=True
+for i in s:
+  if i>=mod or i<=1:
+    check=False
+    break
+check=check & len(s)==len(lst)
+check=check & len(lst)==len(set(lst))
+
+cs=""
+for i in s:
+  if pow(i,small,mod)==1:
+    cs+="1"
+  else:
+    cs+="0"
+check=check & cs==string
 
 if check:
   with open('flag.txt','rb') as f:
