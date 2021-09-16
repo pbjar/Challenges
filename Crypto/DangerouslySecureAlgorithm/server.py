b7db2bc4dd1e857332582d6d4af89fe71b97c0f1
bever209 <74867461+bever209@users.noreply.github.com>
2021-09-16 13:56:14 -0400

    Update server.py

--- Crypto/DangerouslySecureAlgorithm/server.py
+++ Crypto/DangerouslySecureAlgorithm/server.py
@@ -1,47 +1,42 @@
 from hashlib import *
 from Crypto.Util.number import *
-from Crypto.PublicKey import *
-from secrets import *
+from Crypto.PublicKey import DSA
+from random import *
+
+seed(821128091212098832321879138920812390123809132890)
 
 def tryagain():
   print ("I'm so sorry, please try again :(")
   exit()
 
 def H(msg):
-  return bytes_to_long(sha256(msg).hexdigest())
+  return sha256(msg).hexdigest()
 
 
-msg="woahwoahnicej0bwowm4ybeth1s1sn0ts0dangerous"
+msg=b"woahwoahnicej0bwowm4ybeth1s1sn0ts0dangerous"
 
 l=1024
 n=160
 dsa=DSA.generate(1024)
-p = dsakey.p
-q = dsakey.q
-h=randbelow(p-1)
-  if h<2:
-    tryagain()
-  g=pow(h,(p-1)/q,p)
-  if g!=1:
-    return g
-  else:
-    tryagain()
+p = dsa.p
+q = dsa.q
+h=randint(2,p-2)
+g=pow(h,(p-1)//q,p)
+if g==1:
+  tryagain()
 
 print (p)
 print (q)
 print (g)
 
-x=randbelow(q)
-if x==0:
-  tryagain()
- 
+x=randint(1,q-1) 
 y=pow(g,x,p)
 print (y)
 
 xor=0
-hash=H(msg)
+hash=int(H(msg),16)
 for i in range(2):
-  k=randbelow(q)^xor
+  k=randint(1,q-1)^xor
   if k==0:
     tryagain()
   r=pow(g,k,p)
@@ -55,8 +50,8 @@ for i in range(2):
   xor=k
   
 print ("OK, now if you guess my message, I will give you the flag!")
-s=input("Enter: ").strip().decode("utf-8") 
-if msg==s:
+s=input("Enter: ").strip()
+if msg.decode('utf-8')==s:
   with open('flag.txt','rb') as g:
     flag = g.read().strip()
   print (flag)
